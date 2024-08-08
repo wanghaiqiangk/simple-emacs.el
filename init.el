f0eaef1fe4d4f3777310d9ba5f57ea05ffb91bc5
Wang Haiqiang <haiqiang.wang@uisee.com>
2024-08-08 11:33:44 +0800

    Change to non-regex isearch

--- init.el
+++ init.el
@@ -63,11 +63,6 @@
   (global-set-key (kbd "C-x C-b") 'ibuffer)
   (global-set-key (kbd "M-z") 'zap-up-to-char)
 
-  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-  (global-set-key (kbd "C-M-s") 'isearch-forward)
-  (global-set-key (kbd "C-M-r") 'isearch-backward)
-
   (show-paren-mode 1)
   (electric-pair-mode 1)
   (setq-default indent-tabs-mode nil)
